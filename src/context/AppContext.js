f968fd7dba6f4801f3d8aba9eda8a528dfb43615
Radwa-Ibrahim0 <137509740+Radwa-Ibrahim0@users.noreply.github.com>
2023-11-09 15:20:46 +0200

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -66,6 +66,7 @@ export const AppReducer = (state, action) => {
             };
         case 'CHG_CURRENCY':
             action.type = "DONE";
+
             state.currency = action.payload;
             return {
                 ...state
